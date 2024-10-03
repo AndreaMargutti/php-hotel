574c1c6e4ef13a278f2165bd1b99fbb38a22ad6f
AndreaMargutti <marguttiandrea30@gmail.com>
2024-10-03 15:26:22 +0200

    added color at text-name

--- index.php
+++ index.php
@@ -66,7 +66,7 @@
             <tbody>
                 <?php  foreach($hotels as $hotel) {?>
                 <tr>
-                    <th scope="row"><?php echo $hotel["name"]; ?></th>
+                    <th scope="row" class="text-info"><?php echo $hotel["name"]; ?></th>
                     <td><?php echo $hotel["description"]; ?></td>
                     <td><?php echo ($hotel["parking"] ? 'yes' : 'no'); ?></td>
                     <td><?php echo $hotel["vote"]; ?></td>
